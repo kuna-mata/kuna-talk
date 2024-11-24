d498ec28c1d56daf0d0108fdaa242e878e2b3ccd
ha-ccoon <codeminhasohn@gmail.com>
2024-11-24 20:00:24 +0900

    chore: create chat room UI

--- lib/chat.dart
+++ lib/chat.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:kuna_talk/api/message.dart';
 import 'package:kuna_talk/dto/create_message_dto.dart';
+import 'package:kuna_talk/dto/fetch_message_dto.dart';
+import 'package:kuna_talk/models/message.dart';
 import 'package:kuna_talk/utils/util.dart';
 
 class Chat extends StatefulWidget {
@@ -12,51 +14,116 @@ class Chat extends StatefulWidget {
 }
 
 class ChatState extends State<Chat> {
+  final person1 = '4rwodzn1uuacfpq';
+  final person2 = 'c4w28yb8dmgo4lz';
+
+  final TextEditingController controller = TextEditingController();
+  List<Message> messages = [];
+  String? message;
+
   @override
-  Widget build(BuildContext context) {
-    final TextEditingController controller = TextEditingController();
+  void initState() {
+    super.initState();
+    loadMessages();
+  }
 
-    Future<void> sendMessage(String message) async {
-      if (message.isEmpty) {
-        print('Message required');
-        return;
-      }
-
-      final data = CreateMessageDto(
-        senderId: generateRandomId(15),
-        receiverId: generateRandomId(15),
-        messageId: generateRandomId(15),
-        message: message,
-        updatedAt: DateTime.now(),
-        createdAt: DateTime.now(),
+  Future<void> loadMessages({DateTime? createdAt}) async {
+    try {
+      final response = await fetchMessage(
+        FetchMessageDto(
+          senderId: person1,
+          receiverId: person2,
+          createdAt: createdAt!,
+        ),
       );
 
-      await createMessage(data);
+      setState(() {
+        messages.addAll(response);
+      });
+    } catch (e) {
+      throw Exception('loadMessages error: $e');
+    }
+  }
+
+  Future<void> sendMessage(String message) async {
+    if (message.isEmpty) {
+      print('Message required');
+      return;
     }
 
+    final data = CreateMessageDto(
+      senderId: person1,
+      receiverId: person2,
+      messageId: generateRandomId(15),
+      message: message,
+      updatedAt: DateTime.now(),
+      createdAt: DateTime.now(),
+    );
+
+    try {
+      final response = await createMessage(data);
+
+      setState(() {
+        messages.add(response);
+        message = response.message;
+        controller.clear();
+      });
+
+      loadMessages(createdAt: response.createdAt);
+    } catch (e) {
+      throw Exception('sendMessage error: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: TextFormField(
-                controller: controller,
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Type messages',
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                await sendMessage(controller.text);
-              },
-              child: const Text('Send'),
-            )
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text('Kuna Talk'),
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.separated(
+              itemCount: messages.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(messages[index].message),
+                  subtitle: Text(messages[index].senderId),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(height: 10.0),
+            ),
+          ),
+          Container(
+            color: const Color.fromARGB(255, 183, 168, 123),
+            height: 100,
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: controller,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Type messages',
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      sendMessage(controller.text);
+                    });
+                  },
+                  child: const Text('Send'),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+      resizeToAvoidBottomInset: true,
     );
   }
 }
